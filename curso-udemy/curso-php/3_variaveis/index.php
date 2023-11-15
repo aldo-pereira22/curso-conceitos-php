81adc1d2b2f43360eca0afc5d81814823297614e
Aldo <aldopereira.gyn@gmail.com>
2023-11-15 10:27:00 -0300

    PHP: Variáveis

---
+++ curso-udemy/curso-php/3_variaveis/index.php
@@ -0,0 +1,75 @@
+<?php
+    // $teste = "Algum texto";
+
+    // echo $teste;
+
+    // $num = 10;
+
+    // echo "<br>";
+    // echo $num;
+
+
+    $velocidade = 100;
+    $marca = "Ferari";
+    $itens = [
+        "Teto solar",
+        "Motor 2.0",
+        "Porta malas grande",
+        "Piloto automatico",
+    ];
+
+    echo "<br>";
+
+    echo $velocidade;
+    echo "<br>";
+
+    echo $marca;
+    echo "<br>";
+    print_r($itens);
+
+    echo "<br>";
+
+
+    $a = 10;
+    $b = 99.324;
+
+    $c = $a + $b;
+
+
+    echo $c;
+
+    echo "Variável de variavel";
+
+
+    $x = "nome";
+
+    echo "$x <br>";
+
+    $$x = "Aldo";
+
+
+    echo "$nome";
+    echo "<br> ";
+    echo $x;
+    echo "<br> ";
+    echo "<br> ";
+
+
+    echo "Variável por referência";
+
+    $x = 20;
+    $y = & $x;
+
+    echo "<br> ";
+    echo "<br> ";
+
+    echo $x;
+    echo "<br> ";
+    echo $y;
+
+?>
+
+
+
+
+
