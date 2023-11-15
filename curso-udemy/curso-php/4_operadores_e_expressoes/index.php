1f75ba425107e1dad50e04bdebff8a4207729128
Aldo <aldopereira.gyn@gmail.com>
2023-11-15 10:49:15 -0300

    PHP: Operadores

---
+++ curso-udemy/curso-php/4_operadores_e_expressoes/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    echo 3 + 2 * 5;
+    echo "<br>";
+    echo (3+5) * 5;
+    
+
+    echo "<br>";
+    echo 5 +2  /10;
+
+    echo "<br>";
+    echo (5 +2)  /10;
+
+
+    $a = 5;
+    $b = 2;
+    $c = 10;
+
+    echo "<br>";
+    echo $a + $b / $c;
+
+?>
