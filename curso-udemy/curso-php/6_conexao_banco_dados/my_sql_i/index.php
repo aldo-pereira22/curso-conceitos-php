65b9ba12db16f62d9bc98d04331f440bcf8541af
Aldo <aldopereira.gyn@gmail.com>
2023-11-18 21:39:45 -0300

    PHP: Alteração de dados com MSQLI

--- curso-udemy/curso-php/6_conexao_banco_dados/my_sql_i/index.php
+++ curso-udemy/curso-php/6_conexao_banco_dados/my_sql_i/index.php
@@ -27,12 +27,86 @@
 
     // $conn->close();
 
-
-    $table = "itens";
-    $nome = "Xicara";
+    // $table = "itens";
+    // $nome = "Xicara";
     // $descricao = "É uma Xicara usada na cor rosa";
 
-    $q = "INSERT INTO $table (nome) VALUES ($nome)";
+    // $q = "INSERT INTO $table (nome, descricao) VALUES ('$nome', '$descricao')";
     
-    $conn->query($q);
-    $conn->close();
+    // $conn->query($q);
+    // $conn->close();
+
+
+
+    //  Fazer seleção no banco de dados
+
+
+    // $table = "itens";
+    // $nome = "Xicara";
+    // $descricao = "É uma Xicara usada na cor rosa";
+
+    // $q = "INSERT INTO $table (nome, descricao) VALUES ('$nome', '$descricao')";
+//     $q = "SELECT * FROM itens";
+//     $result = $conn->query($q);
+
+//     $conn->close();
+
+// // Retorna um resultado
+//     $item = $result->fetch_assoc();
+
+
+//     // Retorna todos os resultados
+
+//     $itens = $result->fetch_all();
+//     print_r($itens);
+
+
+// $nome = "Celular XIAMI";
+// $descricao = "Celular Usado com a tela quebrada!";
+// $stmt = $conn->prepare("INSERT INTO itens (nome, descricao) VALUES (?, ?)");
+
+// $stmt->bind_param("ss", $nome, $descricao); // s = String, i = Integer, d = double;
+// $stmt->execute();
+
+
+
+
+// $nome = "NOTEBOOK";
+
+// $stmt = $conn->prepare("SELECT * FROM itens WHERE nome= ?");
+// $stmt->bind_param("s", $nome);
+// $stmt->execute();
+
+// $result = $stmt->get_result();
+
+// $data = $result->fetch_all();
+
+// print_r($data);
+
+
+
+// $nome = "NOTEBOOK";
+// $descricao = "NOTEBOOK LENOVO I7 40GB";
+// $stmt = $conn->prepare("UPDATE itens SET descricao = ? WHERE nome = ?");
+// $stmt->bind_param("ss", $descricao, $nome);
+
+// $stmt->execute();
+// if($stmt->error){
+//     echo "ERRO: " . $stmt->error;
+// }
+
+
+
+$descricao="É uma Xicara usada na cor rosa";
+$stmt = $conn->prepare("DELETE FROM itens WHERE descricao = ?");
+$stmt->bind_param("s", $descricao);
+$stmt->execute();
+$conn->close();
+
+
+
+
+
+
+
+
