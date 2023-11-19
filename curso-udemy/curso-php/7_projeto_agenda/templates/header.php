c3b20237b49ae09b1a5f922ea8862f628b78c3bf
Aldo <aldopereira.gyn@gmail.com>
2023-11-18 23:39:01 -0300

    Projeto Agenda: Configurando projeto

---
+++ curso-udemy/curso-php/7_projeto_agenda/templates/header.php
@@ -0,0 +1,33 @@
+<?php
+
+    include_once("config/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contatos</title>
+    <!-- BootStrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.min.css"
+        integrity="sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- FONT AWESOME -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+            integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+    <header>
+
+        <nav class="navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?= $BASE_URL ?>7_projeto_agenda">
+                <img src="/img/logo.svg" alt="Agenda">
+            </a>
+        </nav>
+    </header>
