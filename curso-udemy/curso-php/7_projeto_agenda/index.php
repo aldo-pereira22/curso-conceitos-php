113cccb528e9fccf104bd717af8cdae7641422c7
Aldo <aldopereira.gyn@gmail.com>
2023-11-19 14:43:04 -0300

    Finalizando CSS da Home

--- curso-udemy/curso-php/7_projeto_agenda/index.php
+++ curso-udemy/curso-php/7_projeto_agenda/index.php
@@ -28,8 +28,8 @@
                                 <td scope="row"> <?= $contact['phone'] ?> </td>
                                 <td class="actions">
                                     <a href="#"> <i class="fas fa-eye check-icon"> </i> </a>
-                                    <a href="#"> <i class="fas fa-edit check-icon"> </i> </a>
-                                    <button type="submit"> <i class="fas fa-times delete-icon"></i></button>
+                                    <a href="#"> <i class="fas fa-pencil edit-icon"> </i> </a>
+                                    <button type="submit" class="delete-btn"> <i class="fas fa-times delete-icon"></i> </button>
                                 </td>
 
                             </tr>
