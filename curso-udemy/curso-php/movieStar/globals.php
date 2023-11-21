b616d32c10fa0421c0f7eb214f8b0f8de40ebc36
Aldo <aldopereira.gyn@gmail.com>
2023-11-20 23:25:15 -0300

    Estrutura do Site: Navbar, Body, Footer

--- curso-udemy/curso-php/movieStar/globals.php
+++ curso-udemy/curso-php/movieStar/globals.php
@@ -1,5 +1,6 @@
 <?php
 
     session_start();
-    $BASE_URL = "http://" . $_SERVER["SERVER_NAME"] . dirname($_SERVER["REQUEST_URI"]."?") . "/";
+    // $BASE_URL = "http://" . $_SERVER["SERVER_NAME"] . dirname($_SERVER["REQUEST_URI"]."?") ;
+    $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] .':8081' . dirname($_SERVER['REQUEST_URI'] . '?') ;
 ?>
