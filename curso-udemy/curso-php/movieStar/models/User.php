6bfc3e59eb4026e0ba2a882d97f293a6bdefe618
Aldo <aldopereira.gyn@gmail.com>
2023-11-24 23:05:42 -0300

    Finalizando a home

---
+++ curso-udemy/curso-php/movieStar/models/User.php
@@ -0,0 +1,17 @@
+<?php
+
+    class User {
+
+        public $id;
+        public $name;
+        public $lastname;
+        public $email;
+        public $password;
+        public $imagem;
+        public $bio;
+        public $token;
+
+
+
+    }
+?>
